562230ab745fe9ed891eb88eebfd4a85eddc650c
anshultamrakar <anshultamrakar487@gmail.com>
2021-11-02 07:50:02 -0400

    Update DarkMode.js

--- react-practice-basics/src/DarkMode.js
+++ react-practice-basics/src/DarkMode.js
@@ -2,11 +2,9 @@ import React, { useState } from 'react';
 
 const DarkMode = () => {
 
-    const [darkMode ,setDarkMode] = useState(false)
     return (
-        <main style = {{backgroundColor: "black"}}>
-            <h3> Dark mode Functionality </h3>
-            <button> Toggle </button>
+        <main>
+           
         </main>
     )
 }
